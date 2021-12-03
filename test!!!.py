73f19bdb3f853798e8483285dd9c6213557efdd4
Te4hnoPlus <79105099+Te4hnoPlus@users.noreply.github.com>
2021-12-03 15:03:56 +0500

    Update test!!!.py

--- test!!!.py
+++ test!!!.py
@@ -69,3 +69,80 @@ def main():
 
 if __name__ == "__main__":
     main()
+########################################
+
+seed = 1
+def randoPlus():
+    global seed
+    seed = (seed * 31 + 111119) % 10
+    return seed
+
+numbers = {
+    0: ('XXX', 'X X', 'X X', 'X X', 'XXX'),
+    1: ('  X', ' XX', '  X', '  X', '  X'),
+    2: ('XXX', 'X  ', 'XXX', '  X', 'XXX'),
+    3: ('XXX', '  X', ' XX', '  X', 'XXX'),
+    4: ('  X', ' XX', 'XXX', '  X', '  X'),
+    5: ('XXX', 'X  ', 'XXX', '  X', 'XXX'),
+    6: ('XXX', 'X  ', 'XXX', 'X X', 'XXX'),
+    7: ('XXX', '  X', ' X ', ' X ', 'X  '),
+    8: ('XXX', 'X X', 'XXX', 'X X', 'XXX'),
+    9: ('XXX', 'X X', 'XXX', '  X', 'XXX')
+}
+
+class task1(object):
+    def __init__(self):
+        pass
+    def run(self):
+        strings = set()
+        while len(strings) < 4:
+            strings.add(randoPlus())
+        print(strings)
+
+class task2(object):
+    def __init__(self):
+        pass
+    def run(self):
+        global numbers
+        for i in range(len(numbers)):
+            for v in numbers[i]:
+                print(v)
+
+class task3(object):
+    def __init__(self):
+        pass
+    def run(self):
+        print(self.logic(input()))
+
+    def logic(self, value):
+        strings = [""] * len(numbers[0])
+
+        #for v in value:
+        #    index = 0
+        #    for codeNumber in numbers[int(v)]:
+        #        strings[index] += codeNumber + " | "
+        #        index += 1
+        #v = input()
+
+        for s in strings:
+            for v in value:
+            
+            ee = 
+        
+        return "\n".join(strings)
+        
+        #return "\n".join(strings)
+
+def main():
+    key = 3 #int(input())
+    tas1 = task1()
+    if key == 1:
+        tas1 = task1()
+    elif key == 2:
+        tas1 = task2()
+    elif key == 3:
+        tas1 = task3()
+    tas1.run()
+
+if __name__ == "__main__":
+    main()
